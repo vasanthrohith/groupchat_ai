b04673b52cb00ede8c301e64fa0da84667974ecc
“Vasanth_Shankar_N” <vasanth.s@valuehealthsol.com>
2025-02-08 16:14:46 +0530

    working basic model with Ai asnt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from typing import List
+from src.ai_assistant import AI_Assistant
 
 app = FastAPI()
 
@@ -24,23 +25,33 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
+message_history = []
+
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
+    ai_asnt = AI_Assistant()
     try:
         while True:
             data = await websocket.receive_text()
             # await manager.send_message(f"Client {client_id}: {data}", websocket)
             print(f"Client {client_id}: {data}")
-            
-
+            message_history.append(f"{client_id} : {data}")
+    
             await manager.broadcast(f"Client {client_id} says: {data}")
+            
+            # invoking the AI
+            ai_response = ai_asnt.monitor_user_discussion(message_history=message_history, user_recent_message=data)
+            if ai_response!= "skip":
+                await manager.broadcast(f"AI says: {ai_response}")
+
+
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"Client {client_id} disconnected")
 
 @app.get("/", response_class=HTMLResponse)
 async def get():
-    with open("index.html", "r") as f:
+    with open("templates/index.html", "r") as f:
         html_content = f.read()
     return HTMLResponse(content=html_content, status_code=200)
---
+++ src/ai_assistant.py
@@ -0,0 +1,53 @@
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+
+
+load_dotenv(override=True)
+os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+
+# # Initialize Groq LLM
+# llm = ChatGroq(
+#     model_name="llama3-70b-8192",
+#     temperature=0.7
+# )
+
+
+
+class AI_Assistant:
+    def __init__(self,):
+        # Initialize Groq LLM
+        self.llm = ChatGroq(
+            model_name="llama3-70b-8192",
+            temperature=0.7
+        )
+
+   
+    def monitor_user_discussion(self,message_history, user_recent_message):
+        try:
+            if "heyai" in user_recent_message.replace(" ","").lower():
+                response = self.call_assistant(message_history, user_recent_message)
+                return response
+            
+            return "skip"
+        except Exception as error:
+            print("Error : ",error)
+            return "skip"
+
+
+    def call_assistant(self, message_history, user_recent_message):
+        system_prompt = f"""You are an helpful assistant bot helping in group discussion. 
+                        with the below chat message try to assist user's question.
+                        \n\n discussion hustory:\n\n {message_history}
+                        """
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", system_prompt),
+            ("user", "{input}")
+        ])
+
+        # Create the chain that guarantees JSON output
+        assistant_chain = prompt | self.llm 
+        response  = assistant_chain.invoke({"input": user_recent_message})
+
+        return response.content
